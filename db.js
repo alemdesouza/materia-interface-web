976eb4992891897942b3df812ab207903a519d2b
Alessandra Lima <alessandramsouzalima@gmail.com>
2019-05-28 19:53:39 -0300

    tarefa colocar site dinamico

---
+++ db.js
@@ -0,0 +1,21 @@
+var bancofake = {
+  noticias : [{
+    titulo: "Russia's Military Mission Creep Advances to a New Front: Africa",
+    categoria: "politica",
+    data: "05/10/2018",
+    autor: "Zezinho da Silva",
+    imagem: "https://static01.nyt.com/images/2019/03/28/us/politics/00dc-military1/merlin_152734272_746f1df7-7f7c-4a1e-82c5-8f5222d67a27-superJumbo.jpg?quality=90&auto=webp",
+    resenha: "Trata de um noticia na qual relata a situação na Africa",
+    conteudo: "OUAGADOUGOU, Burkina Faso — Russia has been steadily expanding its military influence across Africa, alarming Western officials with increasing arms sales, security agreements and training programs for unstable countries or autocratic leaders. In the Central African Republic, where a Russian has been installed as the president’s national security adviser, the government is selling mining rights for gold and diamonds at a fraction of their worth to hire trainers and buy arms from Moscow. Russia is seeking to ensconce itself on NATO’s southern flank by helping a former general in Libya fight for control over his government and vast oil market. [Sign up for the weekly At War newsletter to receive stories about duty, conflict and consequence.] Sudan’s president, Omar Hassan al-Bashir, brought in Russian mercenaries in January to help shore up his rule against nationwide protests. And last spring, five sub-Saharan African countries — Mali, Niger, Chad, Burkina Faso and Mauritania — appealed to Moscow to help their overtaxed militaries and security services combat the Islamic State and Al Qaeda."
+  }],
+
+  propagandas: [
+    {
+      titulo: "Assinando hoje, você não paga os primeiros 30 dias. Assine já!"
+    },
+    {
+      titulo: "compre 10 revista por R$100.00"
+    }
+
+  ]
+};
